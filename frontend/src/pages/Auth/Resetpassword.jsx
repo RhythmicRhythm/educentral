7369aec9c045602cc666fa673a1ffef51469cc8e
rhythmicrhythm <Okubadejorh@gmail.com>
2023-02-05 18:53:12 +0100

    password auth

--- frontend/src/pages/Auth/Resetpassword.jsx
+++ frontend/src/pages/Auth/Resetpassword.jsx
@@ -7,61 +7,53 @@ import key from "./Assets/KeyIcon.png";
 import Icon from "react-icons-kit";
 import { basic_eye } from "react-icons-kit/linea/basic_eye";
 import { basic_eye_closed } from "react-icons-kit/linea/basic_eye_closed";
-import {arrows_exclamation} from 'react-icons-kit/linea/arrows_exclamation'
-import {arrows_circle_check} from 'react-icons-kit/linea/arrows_circle_check'
-import {radioChecked} from 'react-icons-kit/icomoon/radioChecked';
-import {radioUnchecked} from 'react-icons-kit/icomoon/radioUnchecked'
-
+import { arrows_exclamation } from "react-icons-kit/linea/arrows_exclamation";
+import { arrows_circle_check } from "react-icons-kit/linea/arrows_circle_check";
+import { radioChecked } from "react-icons-kit/icomoon/radioChecked";
+import { radioUnchecked } from "react-icons-kit/icomoon/radioUnchecked";
 
 const Resetpassword = () => {
   const [type, setType] = useState("password");
 
-   // validated states
-   const [lowerValidated, setLowerValidated]=useState(false);
-   const [upperValidated, setUpperValidated]=useState(false);
-   const [numberValidated, setNumberValidated]=useState(false);
-   const [specialValidated, setSpecialValidated]=useState(false);
-   const [lengthValidated, setLengthValidated]=useState(false);
+  // validated states
+  const [lowerValidated, setLowerValidated] = useState(false);
+  const [upperValidated, setUpperValidated] = useState(false);
+  const [numberValidated, setNumberValidated] = useState(false);
+  const [specialValidated, setSpecialValidated] = useState(false);
+  const [lengthValidated, setLengthValidated] = useState(false);
 
-   const handleChange=(value)=>{
-    const lower = new RegExp('(?=.*[a-z])');
-    const upper = new RegExp('(?=.*[A-Z])');
-    const number = new RegExp('(?=.*[0-9])');
-    const special = new RegExp('(?=.*[!@#\$%\^&\*])');
-    const length = new RegExp('(?=.{8,})')
-    if(lower.test(value)){
+  const handleChange = (value) => {
+    const lower = new RegExp("(?=.*[a-z])");
+    const upper = new RegExp("(?=.*[A-Z])");
+    const number = new RegExp("(?=.*[0-9])");
+    const special = new RegExp("(?=.*[!@#$%^&*])");
+    const length = new RegExp("(?=.{4,})");
+    if (lower.test(value)) {
       setLowerValidated(true);
-    }
-    else{
+    } else {
       setLowerValidated(false);
     }
-    if(upper.test(value)){
+    if (upper.test(value)) {
       setUpperValidated(true);
-    }
-    else{
+    } else {
       setUpperValidated(false);
     }
-    if(number.test(value)){
+    if (number.test(value)) {
       setNumberValidated(true);
-    }
-    else{
+    } else {
       setNumberValidated(false);
     }
-    if(special.test(value)){
+    if (special.test(value)) {
       setSpecialValidated(true);
-    }
-    else{
+    } else {
       setSpecialValidated(false);
     }
-    if(length.test(value)){
+    if (length.test(value)) {
       setLengthValidated(true);
-    }
-    else{
+    } else {
       setLengthValidated(false);
     }
-  }
-
-
+  };
 
   return (
     <div>
@@ -89,7 +81,7 @@ const Resetpassword = () => {
                 id="password"
                 placeholder="Password"
                 className="form-input"
-                onChange={(e)=>handleChange(e.target.value)}
+                onChange={(e) => handleChange(e.target.value)}
               />
               {type === "password" ? (
                 <span className="icon-span" onClick={() => setType("text")}>
@@ -104,43 +96,107 @@ const Resetpassword = () => {
 
             {/* Lower Case Validation */}
             <div className="tracker-box pb-2 pt-4 text-left text-xs">
-            <div className={lowerValidated?'validated':'not-validated'}>
-            {lowerValidated?(
-              <span className='list-icon text-blue-500 text-xs mr-1'>
-                <Icon size={12}  icon={radioChecked}/>  
-              </span>
-            ):(
-              <span className='list-icon  text-xs mr-1'>
-                <Icon size={12}  icon={radioUnchecked}/>  
-              </span>
-            )}                                                       
-            At least one lowercase letter 
-          </div>
+              <div className={lowerValidated ? "validated" : "not-validated"}>
+                {lowerValidated ? (
+                  <span className="list-icon text-blue-500 text-xs mr-1">
+                    <Icon size={12} icon={radioChecked} />
+                  </span>
+                ) : (
+                  <span className="list-icon  text-xs mr-1">
+                    <Icon size={12} icon={radioUnchecked} />
+                  </span>
+                )}
+                At least one lowercase letter
+              </div>
             </div>
 
+            {/* Upper Case Validation */}
+            <div className="tracker-box pb-2 pt-4 text-left text-xs">
+              <div className={upperValidated ? "validated" : "not-validated"}>
+                {upperValidated ? (
+                  <span className="list-icon text-blue-500 text-xs mr-1">
+                    <Icon size={12} icon={radioChecked} />
+                  </span>
+                ) : (
+                  <span className="list-icon  text-xs mr-1">
+                    <Icon size={12} icon={radioUnchecked} />
+                  </span>
+                )}
+                At least one uppercase letter
+              </div>
+            </div>
 
+            {/* Number Validation */}
+            <div className="tracker-box pb-2 pt-4 text-left text-xs">
+              <div className={numberValidated ? "validated" : "not-validated"}>
+                {numberValidated ? (
+                  <span className="list-icon text-blue-500 text-xs mr-1">
+                    <Icon size={12} icon={radioChecked} />
+                  </span>
+                ) : (
+                  <span className="list-icon  text-xs mr-1">
+                    <Icon size={12} icon={radioUnchecked} />
+                  </span>
+                )}
+                At least one number
+              </div>
+            </div>
+
+            {/* Special Validation */}
+            <div className="tracker-box pb-2 pt-4 text-left text-xs">
+              <div className={specialValidated ? "validated" : "not-validated"}>
+                {specialValidated ? (
+                  <span className="list-icon text-blue-500 text-xs mr-1">
+                    <Icon size={12} icon={radioChecked} />
+                  </span>
+                ) : (
+                  <span className="list-icon  text-xs mr-1">
+                    <Icon size={12} icon={radioUnchecked} />
+                  </span>
+                )}
+                At least one special character
+              </div>
+            </div>
+
+            {/* length Validation */}
+            <div className="tracker-box pb-2 pt-4 text-left text-xs">
+              <div className={lengthValidated ? "validated" : "not-validated"}>
+                {lengthValidated ? (
+                  <span className="list-icon text-blue-500 text-xs mr-1">
+                    <Icon size={12} icon={radioChecked} />
+                  </span>
+                ) : (
+                  <span className="list-icon  text-xs mr-1">
+                    <Icon size={12} icon={radioUnchecked} />
+                  </span>
+                )}
+                At least one special character
+              </div>
+            </div>
 
             <div className="pb-2 pt-4 relative">
               <input
-                type={type}
+                type="password"
                 name="password"
                 id="confirm_password"
                 placeholder="Confirm Password"
                 className="form-input"
               />
-              {type === "password" ? (
-                <span className="icon-span font-bold" onClick={() => setType("text")}>
-                  <Icon icon={basic_eye_closed} size={18} />
-                </span>
-              ) : (
-                <span className="icon-span" onClick={() => setType("password")}>
-                  <Icon icon={basic_eye} size={18} />
-                </span>
-              )}
             </div>
 
             <div className="w-full mt-20">
-              <button className="ent-btn block w-full p-2 text-lg text-white rounded-lg mb-2">
+              <button
+                disabled={
+                  !(
+                    lowerValidated &&
+                    upperValidated &&
+                    numberValidated &&
+                    specialValidated &&
+                    lengthValidated
+                  )
+                }
+                className="ent-btn block w-full p-2 text-lg text-white rounded-lg mb-2"
+              >
                 {" "}
                 Reset Password
               </button>
