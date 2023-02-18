ffe88a6101f4e02d4e398b086ff2dcede8a85a4f
rhythmicrhythm <Okubadejorh@gmail.com>
2023-02-18 23:32:25 +0100

    Profile page

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -5,6 +5,7 @@ import eduLogo from "../Assets/eduCentralLogo.png";
 import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 import { SET_NAME, SET_USER } from "../../redux/features/auth/authSlice";
 import { getUser } from "../../services/authServices";
+import axios from "axios";
 
 const Profile = () => {
   useRedirectLoggedOutUser("/login");
@@ -13,26 +14,26 @@ const Profile = () => {
   const [profile, setProfile] = useState(null);
 
   useEffect(() => {
-    console.log("Getting user");
+    console.log("Getting use");
 
     async function getUserData() {
       const data = await getUser();
       console.log(data);
 
       setProfile(data);
-      
+
       await dispatch(SET_USER(data));
       await dispatch(SET_NAME(data.firstname));
     }
     getUserData();
   }, [dispatch]);
 
-
   return (
     <div>
       <div className="min-w-screen min-h-screen flex items-center justify-center mt-10">
-        <div className=" max-w-[95rem] px-8 md:px-6">
-          <div className="flex flex-wrap justify-center flex-col md:flex-row gap-10">
+        <div className="px-8 md:px-6">
+          <div className="flex justify-center flex-col md:flex-row gap-10">
+            
             <div className="flex flex-col flex-wrap gap-8 items-center">
               <h1 className="text-2xl font-bold">My Profile </h1>
               <img src={eduLogo} className="w-48" alt="" />
@@ -42,53 +43,49 @@ const Profile = () => {
               <h1 className="text-xl font-bold mb-6">Basic Information</h1>
 
               <div className="mb-2">
-                <h1 className="font-bold text-gray-800 mb-1">FirstName:</h1>
-                <p className=""></p>
+                <h1 className="font-bold mb-1 text-base">FirstName:</h1>
+                <p className="text-sm text-gray-800 font-semibold">{profile?.firstname}</p>
                 <hr />
               </div>
               <div className="mb-2">
-                <h1 className="font-bold text-gray-800 mb-1">LastName:</h1>
-                <p className=""> </p>
+                <h1 className="font-bold mb-1 text-base">LastName:</h1>
+                <p className="text-sm text-gray-800 font-semibold">{profile?.lastname}</p>
                 <hr />
               </div>
               <div className="mb-2">
                 <h1 className="font-bold text-gray-800 mb-1">Gender:</h1>
-                <p className=""> </p>
+                <p className="">{profile?.gender} </p>
               </div>
               <hr />
               <div className="mb-2">
                 <h1 className="font-bold text-gray-800 mb-1">
                   Marital Status:
                 </h1>
-                <p className=""> </p>
+                <p className=""> {profile?.marital_status}</p>
               </div>
 
-              <div className="flex flex-col md:flex-row gap-6 items-center ">
-                <button className="block w-64 p-2 text-lg text-white rounded-lg mb-2"></button>
-              </div>
+              {/* <div className="flex flex-col md:flex-row gap-6 items-center ">
+                <button className="block bg-blue-700 w-64 p-2 text-lg text-white rounded-lg mb-2">
+                  try
+                </button>
+              </div> */}
             </div>
           </div>
 
           <div className="w-full  text-left ">
-              <h1 className="text-xl font-bold mb-6">Contact Information</h1>
+            <h1 className="text-xl font-bold mb-6">Contact Information</h1>
 
-              <div className="mb-2">
-                <h1 className="font-bold text-gray-800 mb-1">Email:</h1>
-                <p className="">Irebami </p>
-                <hr />
-              </div>
-              <div className="mb-2">
-                <h1 className="font-bold text-gray-800 mb-1">Phone:</h1>
-                <p className="">Shoroye </p>
-                <hr />
-              </div>
-               
-              
-
-              <div className="flex flex-col md:flex-row gap-6 items-center ">
-                <button className="block w-64 p-2 text-lg text-white rounded-lg mb-2"></button>
-              </div>
+            <div className="mb-2">
+              <h1 className="font-bold text-gray-800 mb-1">Email:</h1>
+              <p className="">{profile?.email}</p>
+              <hr />
             </div>
+            <div className="mb-2">
+              <h1 className="font-bold text-gray-800 mb-1">Phone:</h1>
+              <p className="">{profile?.phone}</p>
+              <hr />
+            </div>
+          </div>
         </div>
       </div>
     </div>
