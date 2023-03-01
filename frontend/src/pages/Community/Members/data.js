ea3be54f55c51198e653fd09dfabee68e8ea55ea
rhythmicrhythm <Okubadejorh@gmail.com>
2023-03-01 20:04:49 +0100

    menbers

---
+++ frontend/src/pages/Community/Members/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: 1,
+        firstname: "Olaoluwa",
+        lastname: "David",
+        userimg: "https://preview.redd.it/cpslext1vx971.png?auto=webp&s=a67d767ddec283c3490613cdb0b40c180a33daf6",
+        role: "Teacher",
+        email: "Olaoluwa@gmail.com",
+        date: "Shared on 21 Februray 2020",
+    },
+    {
+        id: 2,
+        firstname: "Obi",
+        lastname: "Emmanuel",
+        userimg: "https://preview.redd.it/cpslext1vx971.png?auto=webp&s=a67d767ddec283c3490613cdb0b40c180a33daf6",
+        role: "Teacher",
+        email: "Obi@gmail.com",
+        date: "Shared on 21 Februray 2020",
+    },
+
+];
+
+export default data;
