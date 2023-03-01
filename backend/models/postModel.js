e2e89eab4be00b29da7841988936a1d5f92961c7
rhythmicrhythm <Okubadejorh@gmail.com>
2023-03-02 00:40:29 +0100

    Post Schema

---
+++ backend/models/postModel.js
@@ -0,0 +1,28 @@
+const postSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
