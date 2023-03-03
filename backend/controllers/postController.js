2f1586f12ab00ca47aca9f40fc697008c74550e8
rhythmicrhythm <Okubadejorh@gmail.com>
2023-03-04 00:36:13 +0100

    updated Get Posts Users

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -39,19 +39,19 @@ const getPosts = asyncHandler(async (req, res) => {
   });
 
 //    // Get single product
-const getPost = asyncHandler(async (req, res) => {
+const getPostUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id);
 
   // Log the ID of the user who added the current user
   const posts = await Post.find({ author: user.addedBy }).sort("-createdAt");
 
   res.status(200).json(posts);
-})
+});
 
 
 
  module.exports = {
     createPost,
     getPosts,
-    getPost
+    getPostUser
   };
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { createPost, getPosts, getPost } = require("../controllers/postController");
+const { createPost, getPosts, getPostUser } = require("../controllers/postController");
 const protect = require("../middleWare/authMiddleware");
 const router = express.Router();
 
 router.post("/", protect, createPost);
 router.get("/getposts", protect, getPosts);
-router.get("/getpost", protect, getPost);
+router.get("/getpostuser", protect, getPostUser);
 
 
 module.exports = router;
