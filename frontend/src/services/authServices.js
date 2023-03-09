b74511c9ea0700b7a92c06886cae47c6fa1c7faf
rhythmicrhythm <Okubadejorh@gmail.com>
2023-03-09 12:54:51 +0100

    +

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const BACKEND_URL = "https://educentral-community-backend.onrender.com";
+// export const BACKEND_URL = "https://educentral-community-backend.onrender.com";
 
-// export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "http://localhost:5000";
 
 export const validateEmail = (email) => {
   return email.match(
@@ -52,46 +52,6 @@ export const loginUser = async (userData) => {
   }
 };
 
-// Add Post
-export const createPost = async(postData) => {
-  try {
-    const response = await axios.post(
-      `${BACKEND_URL}/api/posts/`,
-      postData
-    );
-    if (response.statusText === "OK") {
-      toast.success("post added Successful...");
-    }
-    return response.data;
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    toast.error(message);
-  }
-}
-
-// Add Comment
-export const addComment = async(postData, postId) => {
-  try {
-    const response = await axios.post(
-      `${BACKEND_URL}/api/posts/addcomment/${postId}`,
-      postData
-    );
-    if (response.statusText === "OK") {
-      toast.success("post added Successful...");
-    }
-    return response.data;
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    toast.error(message);
-  }
-}
-
 // Get Login Status
 export const getLoginStatus = async () => {
   try {
@@ -119,78 +79,6 @@ export const logoutUser = async () => {
   }
 };
 
-// Get User Profile
-export const getUser = async () => {
-  try {
-    const response = await axios.get(`${BACKEND_URL}/api/users/getuser`);
-    return response.data;
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    toast.error(message);
-  }
-};
-
-// Get Members
-export const getMembers = async () => {
-  try{
-    const response = await axios.get(`${BACKEND_URL}/api/users/getmembers`);
-    return response.data.members;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Forgot Password
-export const addMember = async (userData) => {
-  try {
-    const response = await axios.post(
-      `${BACKEND_URL}/api/users/addmembers`,
-      userData
-    );
-    toast.success(response.data.message);
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    toast.error(message);
-    
-  }
-};
-
-// Get All Posts
-export const getPosts = async () => {
-  try{
-    const response = await axios.get(`${BACKEND_URL}/api/posts/getposts`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-// Get Posts under Admin
-export const getPostUser = async () => {
-  try{
-    const response = await axios.get(`${BACKEND_URL}/api/posts/getpostuser`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-//Get Single Post
-export const getPostById = async (postId) => {
-  try {
-    const response = await axios.get(`${BACKEND_URL}/api/posts/getpost/${postId}`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-
 // Forgot Password
 export const forgotPassword = async (userData) => {
   try {
@@ -205,7 +93,6 @@ export const forgotPassword = async (userData) => {
       error.message ||
       error.toString();
     toast.error(message);
-    
   }
 };
 
@@ -223,5 +110,115 @@ export const resetPassword = async (userData, resetToken) => {
       error.message ||
       error.toString();
     toast.error(message);
+  }
+};
+
+// Get User Profile
+export const getUser = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/users/getuser`);
+    return response.data;
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
+  }
+};
+
+// addMember
+export const addMember = async (userData) => {
+  try {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/users/addmembers`,
+      userData
+    );
+    toast.success(response.data.message);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
+  }
+};
+
+// Get Members
+export const getMembers = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/users/getmembers`);
+    return response.data.members;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Add Post
+export const createPost = async (postData) => {
+  try {
+    const response = await axios.post(`${BACKEND_URL}/api/posts/`, postData);
+    if (response.statusText === "OK") {
+      toast.success("post added Successful...");
+    }
+    return response.data;
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
+  }
+};
+
+// Add Comment
+export const addComment = async (postData, postId) => {
+  try {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/posts/addcomment/${postId}`,
+      postData
+    );
+    if (response.statusText === "OK") {
+      toast.success("post added Successful...");
+    }
+    return response.data;
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
+  }
+};
+
+// Get All Posts
+export const getPosts = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/posts/getposts`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get Posts under Admin
+export const getPostUser = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/posts/getpostuser`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Get Single Post
+export const getPostById = async (postId) => {
+  try {
+    const response = await axios.get(
+      `${BACKEND_URL}/api/posts/getpost/${postId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
 };
