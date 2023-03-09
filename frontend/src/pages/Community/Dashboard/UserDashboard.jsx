4796b72b34f9410957611ec00f4baaa570025423
rhythmicrhythm <Okubadejorh@gmail.com>
2023-03-09 15:35:32 +0100

    userdashboard+

--- frontend/src/pages/Community/Dashboard/UserDashboard.jsx
+++ frontend/src/pages/Community/Dashboard/UserDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getPostUser } from "../../../services/authServices";
 import Icon from "react-icons-kit";
 import { bubble2 } from "react-icons-kit/icomoon/bubble2";
@@ -28,9 +29,10 @@ const UserDashboard = () => {
         <div className="w-[25rem] md:w-[45rem]">
           <div className="p-6">
             {posts.map((item) => (
-              <div
+              <Link
+              to={`/dashboard/${item._id}`}
                 key={item._id}
-                className="flex flex-row gap-4 justify-center mt-16 border-b-2 border-gray-300 pb-8 bg-white shadow-lg rounded-lg"
+                 className="flex flex-row gap-4 justify-center mt-16 border-b-2 border-gray-300 pb-8 bg-white shadow-lg rounded-lg"
               >
                 <div className="">
                   <img className="rounded-full w-10" src={item.userimage} alt="" />
@@ -49,18 +51,9 @@ const UserDashboard = () => {
                       alt=""
                     />
                   </div>
-                  <div className="flex mt-6 gap-6">
-                    <h1 className="text-xs text-gray-400">
-                      {" "}
-                      <Icon icon={bubble2} size={12} /> 30
-                    </h1>
-                    <h1 className="text-xs text-gray-400">
-                      {" "}
-                      <Icon icon={ic_favorite_border_outline} size={15} /> 30
-                    </h1>
-                  </div>
+                
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
