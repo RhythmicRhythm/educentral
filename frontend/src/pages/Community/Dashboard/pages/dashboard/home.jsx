c5fa80364faa029f525d51e08f598609a114b49f
rhythmicrhythm <Okubadejorh@gmail.com>
2023-04-05 13:34:43 +0100

    +

--- frontend/src/pages/Community/Dashboard/pages/dashboard/home.jsx
+++ frontend/src/pages/Community/Dashboard/pages/dashboard/home.jsx
@@ -173,7 +173,6 @@ export function Home() {
   return (
     <div className="mt-8 md:px-20">
       <div className="mb-4 grid grid-cols-1 gap-8 lg:grid-cols-3 xl:grid-cols-3">
-        
         <div className="overflow-hidden lg:col-span-2 xl:col-span-2">
           <div className="insight bg-white p-4 rounded-lg shadow mt-6">
             <div className="">
@@ -248,13 +247,16 @@ export function Home() {
                       />
                     </div>
                     <div className="">
-                      <h1 className="text-sm text-gray-700 font-bold"> {item.name}</h1>
+                      <h1 className="text-sm text-gray-700 font-bold">
+                        {" "}
+                        {item.name}
+                      </h1>
                       <h2 className="text-xs text-gray-500 font-semibold">
                         description description
                       </h2>
                       <p className="text-xs text-gray-400">
-                      {moment(item.createdAt).fromNow()}{" "}
-                        </p>
+                        {moment(item.createdAt).fromNow()}{" "}
+                      </p>
                     </div>
                   </div>
 
@@ -274,40 +276,78 @@ export function Home() {
                   <img className="rounded-lg" src={item.image} alt="" />
 
                   <div className="mt-4 flex flex-row justify-between">
-                    <div className="flex flex-row gap-4">
+                    <div className="flex flex-row gap-2">
                       <div
                         onClick={() => likepost(item._id)}
                         className="flex flex-row cursor-pointer"
                       >
-                        <img
-                          className="w-6"
-                          src="https://img.icons8.com/stickers/256/facebook-like-skin-type-3.png"
-                          alt=""
-                        />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="w-6 h-6 text-blue-600"
+                        >
+                          <path d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z" />
+                        </svg>
                         <h1 className="text-xs mt-1">
-                          {item ? item.likesCount : 0} Likes
+                          {item ? item.likesCount : 0} like
                         </h1>
                       </div>
                       <div
+                        onClick={() => dislikepost(item._id)}
+                        className="flex flex-row cursor-pointer"
+                      >
+                     
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path d="M15.73 5.25h1.035A7.465 7.465 0 0118 9.375a7.465 7.465 0 01-1.235 4.125h-.148c-.806 0-1.534.446-2.031 1.08a9.04 9.04 0 01-2.861 2.4c-.723.384-1.35.956-1.653 1.715a4.498 4.498 0 00-.322 1.672V21a.75.75 0 01-.75.75 2.25 2.25 0 01-2.25-2.25c0-1.152.26-2.243.723-3.218C7.74 15.724 7.366 15 6.748 15H3.622c-1.026 0-1.945-.694-2.054-1.715A12.134 12.134 0 011.5 12c0-2.848.992-5.464 2.649-7.521.388-.482.987-.729 1.605-.729H9.77a4.5 4.5 0 011.423.23l3.114 1.04a4.5 4.5 0 001.423.23zM21.669 13.773c.536-1.362.831-2.845.831-4.398 0-1.22-.182-2.398-.52-3.507-.26-.85-1.084-1.368-1.973-1.368H19.1c-.445 0-.72.498-.523.898.591 1.2.924 2.55.924 3.977a8.959 8.959 0 01-1.302 4.666c-.245.403.028.959.5.959h1.053c.832 0 1.612-.453 1.918-1.227z" />
+                        </svg>
+
+                        <h1 className="text-xs mt-1">
+                          {item ? item.dislikesCount : 0} disike
+                        </h1>
+                      </div>
+                      <Link
+                        to={`/home/${item._id}`}
                         className="flex flex-row
                 "
                       >
-                        <img
-                          className="w-6"
-                          src="https://img.icons8.com/stickers/100/000000/comments--v1.png"
-                          alt=""
-                        />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+
                         <h1 className="text-xs mt-1">
                           {item ? item.comments.length : 0} Comments
                         </h1>
-                      </div>
+                      </Link>
                     </div>
                     <div className="flex flex-row gap-1">
-                      <img
-                        className="w-6"
-                        src="https://img.icons8.com/stickers/256/share.png"
-                        alt=""
-                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M15.75 4.5a3 3 0 11.825 2.066l-8.421 4.679a3.002 3.002 0 010 1.51l8.421 4.679a3 3 0 11-.729 1.31l-8.421-4.678a3 3 0 110-4.132l8.421-4.679a3 3 0 01-.096-.755z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+
                       <h1 className="text-xs mt-1">27 Shares</h1>
                     </div>
                   </div>
@@ -318,9 +358,7 @@ export function Home() {
         </div>
 
         <div className="col-span-1 ">
-        <div className="relative lg:sticky top-8">
-             gymaihrnbibymfau
-        </div>
+          <div className="relative lg:sticky top-8">gymaihrnbibymfau</div>
         </div>
       </div>
     </div>
